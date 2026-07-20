28178ccde51ba41623713b96c8a7cffae50c764d
Naman Dubey <namandubey10@gmail.com>
2026-07-20 21:20:29 +0530

    Fixed DB connection error

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,9 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config({ path : "./.env"});
 
-export const db = new PrismaClient({ 
-    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) 
-});
- 
+const adapter = new PrismaPg({ connectionString : process.env.DATABASE_URL });
+export const client = new PrismaClient({ adapter });    
